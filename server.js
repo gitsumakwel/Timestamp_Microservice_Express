066cd83c3ff3d1cc9792b26c04a0f3a75cb694ca
gitsumakwel <sumakwelkalantiaw@gmail.com>
2021-11-15 17:32:06 +0800

    unix parse as int

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var app = express();
 var bodyParser = require('body-parser');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 
 
@@ -71,44 +71,44 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 *    servers:
 *      - url: https://boilerplate-project-timestamp.gitsumakwel.repl.co
  */
-const getJsonDate = (req,res,next) => {  
+const getJsonDate = (req,res,next) => {
   //regexp to check for letters and format
   const regAlphaNum = new RegExp('[a-z]','gi');
-  const regDate = new RegExp('\\d\\d\\d\\d-\\d\\d-\\d\\d');  
-  const date = req.params.date===undefined? undefined : (req.params.date || "date");  
+  const regDate = new RegExp('\\d\\d\\d\\d-\\d\\d-\\d\\d');
+  const date = req.params.date===undefined? undefined : (req.params.date || "date");
   //empty date
   if (date===undefined){
     res.json( { unix : Math.floor((new Date(Date.now())/1)), utc : new Date(Date.now()).toUTCString() });
     return;
   }
-  //invalid date 
+  //invalid date
   if (regAlphaNum.test(date)) {
     res.json({ error : "Invalid Date" });
     return;
   }
-  
+
   //for valid date
   if (regDate.test(date) || !isNaN(date)) {
     //get unix
     const unix = Math.floor((new Date(date))/1) || date;
-    //get utc          
+    //get utc
     let utc = new Date(date).toUTCString();
-    if (!isNaN(date)) utc = new Date(Number.parseInt(date)).toUTCString();       
-    res.json({ unix: unix, utc: utc});
+    if (!isNaN(date)) utc = new Date(Number.parseInt(date)).toUTCString();
+    res.json({ unix: Number.parseInt(unix), utc: utc});
     return;
   }
   //invalid format
   res.json({ error : "Invalid Date" });
 }
 
-const jsonDate = (req,res) => {  
+const jsonDate = (req,res) => {
    res.json( { unix : Math.floor((new Date(Date.now())/1)), utc : new Date(Date.now()).toUTCString() });
 }
 
 
 const postJsonDate = (req,res) => {
   console.log(req.body);
-  const date = req.body.date || "date";  
+  const date = req.body.date || "date";
   res.json({ date: `${date}`});
 }
 
@@ -118,7 +118,7 @@ app.use('/public',express.static(__dirname + '/public'));
 app.use('/src',express.static(__dirname + '/src'));
 // will be called for any request
 // use for loging request
-app.use(posthandler); 
+app.use(posthandler);
 // use for 'POST' request
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -134,7 +134,7 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
@@ -145,4 +145,3 @@ app.route('/api/:date?').get(getJsonDate);
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-
